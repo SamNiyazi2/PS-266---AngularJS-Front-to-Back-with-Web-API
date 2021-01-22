227d30c404ac724d7f8f88532796ded9da80b873
Sam Niyazi <sam2003@niyazi.com>
2021-01-22 06:14:09 -0600

    Completed  M10-02.

---
+++ PS_APM_Client_266/app/mainCtrl.js
@@ -0,0 +1,36 @@
+﻿
+// 01/22/2021 06:00 am - SSN - [20210122-0530] - [001] - M10-02 - Building a login form
+
+(function () {
+
+    'use strict';
+
+    angular.module("productManagement")
+        .controller("MainCtrl", ["userAccount", MainCtrl]);
+
+    function MainCtrl(userAccount) {
+
+        var vm = this;
+
+        vm.isLoggedIn = false;
+        vm.message = "";
+        vm.userData = {
+            userName: "",
+            email: "",
+            password: "",
+            confirmPassword: ""
+        };
+
+        vm.registerUser = function () {
+
+        }
+
+        vm.login = function () {
+
+        }
+
+
+    }
+
+}());
+
